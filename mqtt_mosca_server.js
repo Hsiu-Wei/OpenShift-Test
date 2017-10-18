a45e8e15d40ac1a09b67d87af02e07e1f138d62c
59c9c3790c1e667baf000056 <wei.hsu@thermaltake.com.tw>
2017-10-18 16:17:20 +0800

    'test1112'

---
+++ mqtt_mosca_server.js
@@ -0,0 +1,170 @@
+
+var mosca = require('mosca');
+
+var settings = {
+  port: process.env.PORT || 1883
+  //backend: ascoltatore
+};
+//紀錄有上線的power
+var onlinePower = new Object();
+
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+		
+	var value = client.id.split("#");
+	console.log('Value.length:' + value.length);
+	if(value.length > 1){
+		
+		var m_id = value[0];
+		var p_id = value[1];
+		
+		var onlinePId = new Array();
+		
+		if(onlinePower[m_id]){
+		onlinePId =  onlinePower[m_id];
+		console.log(onlinePId);
+		onlinePId.push(p_id);
+		onlinePower[m_id] = onlinePId;	
+		}
+		else{
+			onlinePId.push(p_id);
+			onlinePower[m_id] = onlinePId;	
+		}
+		
+	}
+	
+	//debug print
+	var keysArr = Object.keys(onlinePower)
+	for(var i = 0; i < keysArr.length; i++){
+		var aa = new Array();
+		aa = onlinePower[keysArr[i]];
+		for(var j = 0; j < aa.length; j++){
+			console.log('onlinePower['+keysArr[i]+']:' + aa[j]);
+		}
+	}	
+});
+
+server.on('clientDisconnected', function(client) {
+    console.log('client Disconnected', client.id);
+	
+	var value = client.id.split("#");
+	console.log('Value.length:' + value.length);
+	if(value.length > 1){
+		
+		var m_id = 'phone_m_id#'+value[0];
+		var p_id = value[1];
+		
+		var msg = {
+                topic: m_id,
+                payload: p_id,
+                qos: 0,
+                retain: false,
+				data:'aaaa'
+            };
+            server.publish(msg, function () {
+                console.log('repeat!  aaa' + msg.topic + ', ' + msg.payload);
+            });
+		
+		var onlinePId = new Array();
+		
+		if(onlinePower[m_id]){
+		onlinePId =  onlinePower[m_id];
+		console.log(onlinePId);
+		
+		var index = onlinePId.indexOf(p_id);
+		
+		if(index > -1){
+			onlinePId.splice(index, 1);
+		}
+		
+		onlinePower[m_id] = onlinePId;	
+		}
+		else{
+			var index = onlinePId.indexOf(p_id);
+		
+			if(index > -1){
+				onlinePId.splice(index, 1);
+			}			
+			onlinePower[m_id] = onlinePId;	
+		}
+	}
+	//debug print
+	var keysArr = Object.keys(onlinePower)
+	for(var i = 0; i < keysArr.length; i++){
+		var aa = new Array();
+		aa = onlinePower[keysArr[i]];
+		for(var j = 0; j < aa.length; j++){
+			console.log('onlinePower['+keysArr[i]+']:' + aa[j]);
+		}
+	}
+	
+});
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('---------------------------------');
+  //console.log('client:', client);
+  console.log('topic:', packet.topic);
+  console.log('payload:', packet.payload.toString());
+
+  //var jsonStr = JSON.stringify(packet.payload);
+  var json;// = JSON.parse(packet.payload);
+
+	try {
+		json = JSON.parse(packet.payload);
+
+		console.log('JSON:',json);
+		//console.log('JSON.clientId:',json.clientId);
+		//console.log('JSON.topic:',json.topic);
+		console.log('JSON.p_id:',json.p_id);
+		console.log('JSON.status:',json.status);
+		console.log('JSON.m_id:',json.m_id);
+		
+		if(packet.topic == 'PC_Remote'){
+			console.log('catch PC Remote');
+			
+			var msg = {
+                topic: json.p_id,
+                payload: packet.payload,
+                qos: 0,
+                retain: false
+            };
+            server.publish(msg, function () {
+                console.log('repeat!  aaa');
+            });
+			
+		} 
+		else if(packet.topic == 'Phone'){
+			console.log('catch Phone');
+			
+			var msg = {
+						topic: json.m_id,
+						payload: onlinePower[json.m_id],
+						qos: 0,
+						retain: false
+					};
+					
+				    server.publish(msg, function () {
+						console.log('repeat!  connect status');
+					});
+			
+		}else{
+			
+		}
+		  console.log('---------------------------------');
+
+	} catch (e) {
+		console.log("not JSON");
+	}
+
+  
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
